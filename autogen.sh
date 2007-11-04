51b3672542cc2f8b0c4b4922c7b8ed62a63d5b94
Priyank <priyank@frugalware.org>
2007-11-04 13:55:55 +0530

    autogen.sh * added missing intltoolize and libtoolize commands

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,11 @@
 #!/bin/sh -e
 
+intltoolize -c -f --automake
+libtoolize -f -c
 aclocal --force
 autoheader -f
 autoconf -f
 cp -f $(dirname $(which automake))/../share/automake/mkinstalldirs ./
 cp -f $(dirname $(which automake))/../share/gettext/config.rpath ./
 automake -a -c --gnu --foreign
+
