b94fbf16dab797675faddb0b4284926d74e5d902
Priyank <priyank@frugalware.org>
2007-11-04 13:19:00 +0530

    fun.h: initial import

---
+++ src/fun.h
@@ -0,0 +1,14 @@
+#ifndef __FUN_H__
+#define __FUN_H__
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <locale.h>
+#include <libintl.h>
+#include <gtk/gtk.h>
+
+#define _(string) gettext (string)
+
+#endif
