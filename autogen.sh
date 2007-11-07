c75113c10720678c65e0bc5c446cd7fafdb26203
Miklos Vajna <vmiklos@frugalware.org>
2007-11-08 00:41:02 +0100

    updated autogen.sh for pootle usage

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,63 @@
 #!/bin/sh -e
 
+gen_pot()
+{
+	cd po/
+	mv Makevars Makevars.tmp
+	cp /usr/bin/intltool-extract ./
+	intltool-update --pot --gettext-package=fun
+	rm intltool-extract
+	mv Makevars.tmp Makevars
+	cd - >/dev/null
+}
+
+import_pootle()
+{
+	po_dir=~/git/translations/po
+	if [ -d $po_dir ]; then
+		: > po/LINGUAS
+		for i in $(/bin/ls $po_dir/fun)
+		do
+			[ -e $po_dir/fun/$i/fun.po ] || continue
+			cp $po_dir/fun/$i/fun.po po/$i.po
+			if msgfmt -c --statistics -o po/$i.gmo po/$i.po; then
+				echo $i >> po/LINGUAS
+			else
+				echo "WARNING: po/$i.po will break your build!"
+			fi
+		done
+	else
+		echo "WARNING: no po files will be used"
+	fi
+}
+
+cd `dirname $0`
+
+if [ "$1" == "--pot-only" ]; then
+	gen_pot
+	exit 0
+fi
+
+if [ "$1" == "--dist" ]; then
+	ver=`grep AC_INIT configure.ac|sed 's/.*, \([0-9\.]*\), .*/\1/'`
+	git-archive --format=tar --prefix=fun-$ver/ HEAD | tar xf -
+	git log --no-merges |git name-rev --tags --stdin > fun-$ver/ChangeLog
+	cd fun-$ver
+	./autogen.sh --git
+	cd ..
+	tar czf fun-$ver.tar.gz fun-$ver
+	rm -rf fun-$ver
+	gpg --comment "See http://ftp.frugalware.org/pub/README.GPG for info" \
+		-ba -u 20F55619 fun-$ver.tar.gz
+	#mv gnetconfig-$ver.tar.gz.asc ../..
+	exit 0
+fi
+
+cat /usr/share/aclocal/libtool.m4 >> aclocal.m4
+
 intltoolize -c -f --automake
+import_pootle
+gen_pot
 libtoolize -f -c
 aclocal --force
 autoheader -f
@@ -8,4 +65,8 @@ autoconf -f
 cp -f $(dirname $(which automake))/../share/automake/mkinstalldirs ./
 cp -f $(dirname $(which automake))/../share/gettext/config.rpath ./
 automake -a -c --gnu --foreign
+
+if [ "$1" == "--git" ]; then
+	rm -rf autom4te.cache
+fi
 
