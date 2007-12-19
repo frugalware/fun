0d6c01307385a1f76811421ab269f6a7932d8fc3
Priyank <priyank@frugalware.org>
2007-12-20 01:22:54 +0530

    updated copyright info

--- src/fun-tooltip.c
+++ src/fun-tooltip.c
@@ -2,8 +2,6 @@
  *  fun-tooltip.c for fun
  *
  *  Copyright (C) 2007 by Priyank Gosalia <priyankmg@gmail.com>
- *  Portions of this code are borrowed from gimmix
- *  gimmix is Copyright (C) 2006-2007 Priyank Gosalia <priyankmg@gmail.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
