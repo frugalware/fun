64c55cba40a794d3ad79ce024adce60c79e4a28f
Priyank <priyank@frugalware.org>
2008-03-29 19:36:26 +0530

    Fun News Notification : Initial Import

---
+++ src/fun-news_backend.c
@@ -0,0 +1,395 @@
+/*
+ *  fun-news_backend.c for fun
+ *
+ *  Copyright (C) 2008 by Priyank Gosalia <priyankmg@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include <libxml/xmlreader.h>
+#include "fun-news_backend.h"
+#include "wejpconfig.h"
+
+#define NEWS_ITEM_DIR	".fun/news"
+#define NEWS_ITEM_LIST	".fun/news/list"
+
+/* populated on every update */
+GList	*news_item_list = NULL;
+
+/* populated on startup and updated regularly */
+/* existing news item list */
+GList	*e_news_item_list = NULL;
+
+/**
+ *
+ * fun_newslist_compare_func:
+ *
+ * Compares two lists
+ */
+static gint
+fun_newslist_compare_func (gconstpointer a, gconstpointer b)
+{
+	NewsItem	*item1 = NULL;
+	NewsItem	*item2 = NULL;
+	
+	item1 = (NewsItem*) a;
+	item2 = (NewsItem*) b;
+	if (item1->id == item2->id)
+	{
+		return 0;
+	}
+
+	return 1;
+}
+
+/**
+ *
+ * fun_compare_lists:
+ *
+ * Compares two lists and returns items in newlist
+ * which are not in the old list.
+ * returns NULL if the two lists are equal
+ */
+GList*
+fun_compare_lists (GList *oldlist, GList *newlist)
+{
+	GList *ret = NULL;
+	GList *temp = NULL;
+	
+	temp = newlist;
+	while (temp!=NULL)
+	{
+		GList *found = NULL;
+		NewsItem *ni = NULL;
+		ni = temp->data;
+		
+		found = g_list_find_custom (oldlist, ni, fun_newslist_compare_func);
+		if (found == NULL)
+		{
+			g_print ("LATEST: %d\n", ni->id);
+			ret = g_list_append (ret, temp);
+		}
+		
+		temp = g_list_next (temp);
+	}
+
+	return ret;	
+}
+
+/**
+ *
+ * fun_glist_to_string:
+ *
+ * Converts a GList* to char*
+ */
+static char *
+fun_glist_to_string (GList *list)
+{
+	GString	*new = NULL;
+	char	*ret = NULL;
+	GList	*temp = NULL;
+	guint	lines = 0;
+	
+	new = g_string_new ("");
+	temp = list;
+	lines = g_list_length (temp);
+	while (temp != NULL)
+	{
+		new = g_string_append (new, temp->data);
+		temp = g_list_next (temp);
+	}
+	ret = g_strdup (new->str);
+	g_string_free (new, TRUE);
+	
+	return ret;
+}
+
+/**
+ *
+ * fun_get_news_item_from_file:
+ *
+ * Reads from file and returns NewsItem* containing title and desc
+ */
+static NewsItem*
+fun_get_news_item_from_file (const char *file)
+{
+	FILE *fp = NULL;
+	NewsItem *ni = NULL;
+	char line[PATH_MAX+1] = "";
+	GList *dsclist = NULL;
+	gboolean found = FALSE;
+	
+	if (!(fp=fopen(file,"r")))
+		return NULL;
+	ni = (NewsItem*) malloc (sizeof(NewsItem));
+	memset (ni, 0, sizeof(NewsItem));
+	while (fgets(line,PATH_MAX,fp))
+	{
+		if (!found)
+		{
+			sprintf (ni->title, line);
+			found = TRUE;
+		}
+		else
+		{
+			dsclist = g_list_append (dsclist, g_strdup(line));
+		}
+		memset (line, 0, sizeof(line));
+	}
+	ni->description = fun_glist_to_string (dsclist);
+	g_list_free (dsclist);
+	fclose (fp);
+	
+	return ni;
+}
+
+/**
+ *
+ * fun_populate_existing_news_list:
+ *
+ * Populates a GList of NewsItem* from existing news on disk
+ */
+void
+fun_populate_existing_news_list (void)
+{
+	FILE *fp = NULL;
+	char line[PATH_MAX+1] = "";
+	NewsItem *item = NULL;
+	
+	if (!(fp=fopen(NEWS_ITEM_LIST,"r")))
+		return;
+	while (fgets(line,PATH_MAX,fp))
+	{
+		g_strstrip (line);
+		if (strlen(line))
+		{
+			char *path = g_strdup_printf ("%s/%s", NEWS_ITEM_DIR, line);
+			char *npath = cfg_get_path_to_config_file (path);
+			//g_print ("Getting for %s\n", npath);
+			item = fun_get_news_item_from_file (npath);
+			item->id = atoi (line);
+			g_free (path);
+			g_free (npath);
+			if (item != NULL)
+			e_news_item_list = g_list_append (e_news_item_list, (gpointer)item);
+		}
+	}
+	fclose (fp);
+	
+	return;
+}
+
+/**
+ *
+ * fun_add_entry_to_newslist:
+ * @id: id to be added to list
+ *
+ * Adds id to the news item list
+ */
+static int
+fun_add_entry_to_newslist (gint id)
+{
+	FILE *fp = NULL;
+	
+	if (!(fp=fopen(NEWS_ITEM_LIST,"a")))
+		return -1;
+	fprintf (fp, "%d\n", id);
+	fclose (fp);
+	
+	return 0;
+}
+
+/**
+ *
+ * fun_save_news_to_file:
+ * @item: a pointer to the news item
+ *
+ * Saves a news item to file
+ */
+static int
+fun_save_news_to_file (NewsItem *item)
+{
+	FILE *fp = NULL;
+	char *npath = NULL;
+	char *path = NULL;
+
+	if (item == NULL)
+		return -1;
+	npath = g_strdup_printf ("%s/%d", NEWS_ITEM_DIR, item->id);
+	path = cfg_get_path_to_config_file (npath);
+	g_print ("%s\n", path);
+	fp = fopen (path, "w");
+	if (fp == NULL)
+	{
+		g_print ("Error opening news file\n");
+		return -1;
+	}
+	if (fun_add_entry_to_newslist(item->id)!=0)
+	{
+		g_print ("Couldnt add to new list\n");
+		return -1;
+	}
+	fprintf (fp, "%s\n", item->title);
+	fprintf (fp, "%s\n", item->description);
+	fclose (fp);
+	g_free (path);
+	g_free (npath);
+
+	return 0;
+}
+
+/**
+ *
+ * fun_get_news_id_from_url:
+ * @url: the url
+ *
+ * Extract news id from a url
+ */
+static int
+fun_get_news_id_from_url (const char *url)
+{
+	char *tp = g_strdup (url);
+	char *bk = tp;
+	char *ptr = NULL;
+	char id[4] = "";
+	
+	tp = g_strreverse (tp);
+	ptr = id;
+	while (*tp != '/')
+		*ptr++ = *tp++;
+	*ptr++ = 0;
+	g_free (bk);
+	ptr = id;
+	ptr = g_strreverse (ptr);
+
+	return (atoi(ptr));
+}
+
+/**
+ * processNode:
+ * @reader: the xmlReader
+ *
+ * Dump information about the current node
+ * Parts of this code are based on reader1.c by Daniel Veillard
+ */
+static void
+processNode (xmlTextReaderPtr *reader)
+{
+	const		xmlChar *name, *value;
+	char		*temp = NULL;
+	static int	found = 0;
+	int		i;
+	NewsItem	*newsitem = NULL;
+    
+	name = xmlTextReaderConstName ((*reader));
+	if (name == NULL)
+		name = BAD_CAST "--";
+	if (!found)
+	{
+		if (!strcmp(name,"item"))
+		{
+			found = 1;
+			return;
+		}
+	}
+	else
+	{
+		newsitem = (NewsItem*) malloc (sizeof(NewsItem));
+		memset (newsitem, 0, sizeof(NewsItem));
+		
+		/* Process TITLE */
+		for (i=0;i<2;i++) xmlTextReaderRead ((*reader));
+		value = xmlTextReaderConstValue((*reader));
+
+		if (value == NULL)
+			return;
+		else
+		{
+			temp = g_strdup (value);
+   		 	g_strstrip (temp);
+			if (strlen(temp)>5)
+			{
+				sprintf (newsitem->title, temp);
+				printf ("Title: %s\n", temp);
+				g_free (temp);
+			}
+		}
+		
+		/* Process URL */
+		for (i=0;i<4;i++) xmlTextReaderRead ((*reader));
+		value = xmlTextReaderConstValue((*reader));
+
+		if (value == NULL)
+			return;
+		else
+		{
+			temp = g_strdup (value);
+   		 	g_strstrip (temp);
+			if (strlen(temp)>5)
+			{
+				newsitem->id = getNewsIdFromUrl (temp);
+				printf ("ID: %d\n", newsitem->id);
+				g_free (temp);
+			}
+		}
+		
+		/* Skip GUID */
+		for (i=0;i<4;i++) xmlTextReaderRead ((*reader));
+		
+		/* Process Description */
+		for (i=0;i<4;i++) xmlTextReaderRead ((*reader));
+		value = xmlTextReaderConstValue((*reader));
+
+		if (value == NULL)
+			return;
+		else
+		{
+			temp = g_strdup (value);
+   		 	g_strstrip (temp);
+			if (strlen(temp)>5)
+			{
+				newsitem->description = temp;
+				printf ("Description: %.40s...\n\n\n", temp);
+			}
+		}
+		
+		/* Skip content:encoded description, pubdate etc */
+		for (i=0;i<10;i++) xmlTextReaderRead ((*reader));
+
+		news_item_list = g_list_append (news_item_list, (gpointer)newsitem);
+	}
+
+	return;
+}
+
+/**
+ * fun_backend_init:
+ *
+ * Initializes the fun news notification backend
+ */
+void
+fun_backend_init (void)
+{
+	LIBXML_TEST_VERSION
+
+	/* populate the existing list of news items
+	 * stored on disk */
+	fun_populate_existing_news_list ();
+	e_news_item_list = g_list_reverse (e_news_item_list);
+
+	return;
+}
+
+
---
+++ src/fun-news_backend.h
@@ -0,0 +1,24 @@
+#ifndef __FUN_NEWS_BACKEND_H__
+#define __FUN_NEWS_BACKEND_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <glib.h>
+
+/* the News item structure */
+typedef struct _newsitem {
+	guint	id;
+	char	title[255];
+	char	*description;
+} NewsItem;
+
+void fun_backend_init (void);
+
+void populate_existing_news_list (void);
+
+GList* fun_compare_lists (GList *oldlist, GList *newlist);
+
+
+#endif
+
